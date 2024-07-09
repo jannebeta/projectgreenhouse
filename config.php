e86307158ae80c697182047d6a765d8c3b0e31eb
jannebeta <superkumiankka@gmail.com>
2024-07-09 13:42:21 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,15 +2,11 @@
 
 $databaseHost = 'localhost';
 $databaseUsername = 'root';
-$databasePassword = 'poni123';
+$databasePassword = '';
 $databaseName = 'projectgh';
 
-$secretKey = '76ef127fcbd14daaec50b2bfaf5820c331a81c42';
-
-//$apiUrlInternal = 'http://192.168.100.20/project_greenhouse/jsonapi/';
+$secretKey = 'GENERATE YOUR SECRETKEY TO HERE';
 
 $apiUrl = 'http://192.168.100.20/project_greenhouse/jsonapi/';
-
-//$apiUrl = 'https://janne.ankkaverkko.com/api/';
 
 ?>
